2faa0d3bac62dd2b2aee508736974bc18b8e11c7
mayuribha01-code <mayuribha01@gmail.com>
2026-05-13 20:29:41 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,35 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Chrome()
+
 driver.get("https://www.amazon.in")
 
 driver.maximize_window()
 
-wait = WebDriverWait(driver, 15)
+driver.find_element(By.XPATH, "//input[@id='twotabsearchtextbox']").send_keys("iphones")
 
-# Search box
-search_box = wait.until(
-    EC.presence_of_element_located((By.ID, "twotabsearchtextbox"))
-)
+driver.find_element(By.XPATH, "//input[@id='nav-search-submit-button']").click()
 
-search_box.send_keys("iphones")
-search_box.send_keys(Keys.RETURN)
+list = driver.find_elements(By.XPATH, "//span[@class='a-size-medium a-color-base a-text-normal']")
 
-wait.until(
-    EC.presence_of_element_located((By.CSS_SELECTOR, "div.s-main-slot"))
-)
+print(str(len(list)) + " products found")
 
-products = wait.until(
-    EC.presence_of_all_elements_located((By.XPATH, "//h2//span"))
-)
-
-print(str(len(products)) + " products found")
-
-for i in products:
+for i in list:
     print(i.text)
 
 driver.quit()
